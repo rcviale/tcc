83b6f16ac7dcb8d4b2879d8faaad34bbbf2f9191
rcviale <77018783+rcviale@users.noreply.github.com>
2021-04-27 18:43:11 +0200

    Add factor variables for days of the week and dummies for working days. Reorganize HAR model sequence. Add MedRV with days of the week and working days. Update comparison of all models' prediction errors.

--- binance_intraday.R
+++ binance_intraday.R
@@ -93,6 +93,15 @@ rm(list = ls())
 path <- 'C:\\Users\\rodri\\OneDrive\\Documents\\Academics\\Trabalho de Conclusão de Curso\\'
 coin <- 'BTC'
 
+# Create factor variables for days of the week
+dow <- as.POSIXlt(seq(as.Date(binframe[1, 1]),
+                      as.Date(binframe[nrow(binframe), 1]),
+                      by = "days"))$wday
+# Create dummy variables for working days
+wd <- sapply(1 : length(dow), function(x){
+  ifelse(dow[x] == 0 || dow[x] == 6, 0, 1)
+})
+
 # Load data
 load(paste0(path, 'binframe_', coin, '.RData'))
 
@@ -180,7 +189,7 @@ rm(bpv, tq, lr, medrv)
 # Save data table
 save(ntable, file = paste0(path, 'bintable_', coin, f, '.RData'))
 
-## 5 MIN RETURNS ##
+### 5 MIN RETURNS ###
 f <- 5
 
 # New data table with log difs and indicator functions for positive/negative
@@ -209,11 +218,19 @@ medrv <- tapply(lr[, ret], lr[, date], function(x){
     (pi / (6 - 4 * sqrt(3) + pi)) * (length(x) / (length(x) - 2)) / f
 })
 
+medrv5 <- tapply(lr[, ret], lr[, date], function(x){
+  sum(unlist(lapply(5 : length(x), function(t){
+    return(median(abs(x[(t - 4) : t]), na.rm = TRUE)^2) 
+  })), na.rm = TRUE) * 
+    (pi / (6 - 4 * sqrt(3) + pi)) * (length(x) / (length(x) - 2)) / f
+})
+
 # New data table with all the computed data
 ntable <- lr[, .(ret = sum(ret, na.rm = TRUE) / f, RV = sum(ret^2, na.rm = TRUE) / f, 
                  RSP = sum((ret^2) * Ipos, na.rm = TRUE), RVOL = sqrt(sum(ret^2, na.rm = TRUE)), 
                  RSN = sum((ret^2) * Ineg, na.rm = TRUE), .N), 
-             by = list(date)][, BPV := bpv][, TQ := tq][, medRV := medrv]
+             by = list(date)][, BPV := bpv][, TQ := tq][, medRV := medrv][, medRV5 := medrv5]
+ntable <- ntable[, dow := dow][, wd := wd]
 
 rm(bpv, tq, lr, medrv)
 
@@ -327,74 +344,8 @@ library(data.table)
 # Load the data
 load(paste0(path, 'bintable_', coin, f, '.RData'))
 
-# Plot histogram of the standardized returns
-hist(ntable[, ret], main = 'Distribution of BTC Standardized Log Returns', xlab = NA, prob = TRUE, 
-     breaks = 30, font.main = 1, cex.main = 1, xlim = c(-0.2, 0.2))
-curve(dnorm(x, mean = mean(ntable[, ret]), sd = sd(ntable[, ret])), col = 'darkblue', 
-      lwd = 2, add = TRUE, yaxt = 'n')
-
 # Number of rows in ntable matrix
 L <- nrow(ntable)
-
-# 5-days and 22-days moving average of RV
-nd5 <- unlist(lapply(lapply(5 : L, function(t){return(ntable[(t - 4) : t, RV])}), mean))
-nd22 <- unlist(lapply(lapply(22 : L, function (t) {return(ntable[(t - 21) : t, RV])}), mean))
-
-# 5-days and 22-days moving average of volatility
-rvol5 <- sqrt(nd5) 
-rvol22 <- sqrt(nd22)
-
-rm(nd5, nd22)
-
-L5 <- length(rvol5)
-L22 <- length(rvol22)
-
-# HAR basic model
-HAR <- lm(sqrt(ntable[23 : L, RV]) ~ sqrt(ntable[22 : (L - 1), RV]) + 
-            rvol5[18 : (L5 - 1)] + rvol22[1 : (L22 - 1)])
-summary(HAR)
-
-rm(rvol5, rvol22)
-
-# Histogram of the residuals
-hist(residuals(HAR), breaks = 50, font.main = 1, main = 'Histogram of the Residuals of the HAR Model',
-     prob = TRUE, xlab = NA)
-curve(dnorm(x, mean = mean(residuals(HAR)), sd = sd(residuals(HAR))), col = "darkblue", add = TRUE, lwd = 2)
-
-# Normal Q-Q Plot for the residuals
-qqnorm(residuals(HAR), font.main = 1, cex.main = 1.1,
-       main = 'Normal Q-Q Plot for the Residuals of the HAR Model')
-qqline(residuals(HAR))
-
-# Jarque Bera test for the residuals
-tseries::jarque.bera.test(residuals(HAR))
-
-# ACF of the residuals
-acf(residuals(HAR), main = 'ACF for Residuals of HAR Model', ylab = NA)
-# PACF of the residuals
-pacf(residuals(HAR), main = 'PACF for Residuals of HAR Model', ylab = NA)
-
-# Ljung-Box test for the residuals (H0: independently distributed data)
-Box.test(residuals(HAR), type = 'Ljung-Box', lag = 5)
-
-# Breusch-Pagan test (H0: homoskedasticity)
-lmtest::bptest(HAR)
-
-# Prep moving averages for MedRV
-nd5 <- unlist(lapply(lapply(5 : L, function(t){return(ntable[(t - 4) : t, medRV])}), mean))
-nd22 <- unlist(lapply(lapply(22 : L, function (t) {return(ntable[(t - 21) : t, medRV])}), mean))
-
-medvol5 <- sqrt(nd5) 
-medvol22 <- sqrt(nd22)
-
-rm(nd5, nd22)
-
-# HAR MedRV model
-medHAR <- lm(sqrt(ntable[23 : L, medRV]) ~ sqrt(ntable[22 : (L - 1), medRV]) + 
-            medvol5[18 : (L5 - 1)] + medvol22[1 : (L22 - 1)])
-summary(medHAR)
-
-rm(medvol5, medvol22)
 
 # HAR different time windows comparison
 cols <- 2:28
@@ -419,13 +370,12 @@ for (i in 2 : 27){
                   mai[(j - i + 1) : (Li - 1)] + maj[1 : (Lj - 1)])
     
     # HAR model with days of the week factor
-    use_dow <- dow[1 : L]
     model_d <- lm(sqrt(ntable[(j + 1) : L, RV]) ~ sqrt(ntable[j : (L - 1), RV]) + 
-                  mai[(j - i + 1) : (Li - 1)] + maj[1 : (Lj - 1)] + dow[(j + 1) : L])
+                  mai[(j - i + 1) : (Li - 1)] + maj[1 : (Lj - 1)] + ntable[(j + 1) : L, dow])
     
     # HAR model with working days dummy
     model_w <- lm(sqrt(ntable[(j + 1) : L, RV]) ~ sqrt(ntable[j : (L - 1), RV]) + 
-                    mai[(j - i + 1) : (Li - 1)] + maj[1 : (Lj - 1)] + wd[(j + 1) : L])
+                    mai[(j - i + 1) : (Li - 1)] + maj[1 : (Lj - 1)] + ntable[(j + 1) : L, wd])
     
     # Fill data frame
     crame[(nrow(crame) + 1), 1] <- paste0('(', i, ', ', j, ')')
@@ -447,9 +397,9 @@ colnames(crame) <- c('Spec', 'HAR Rsq', 'HAR BIC', 'HAR-DOW Rsq', 'HAR-DOW BIC',
 View(crame)
 
 save(crame, file = paste0(path, 'crame_', coin, f, '.RData'))
-xlsx::write.xlsx(crame, file = paste0(path, 'crame_', coin, f, '.RData'))
+openxlsx::write.xlsx(crame, file = paste0(path, 'crame_', coin, f, '.xlsx'))
 
-load(paste0(path, 'crame_', coin, f, '.RData'))
+rm(crame)
 
 ### Specific HAR Construction ###
 # Time windows do be used
@@ -477,17 +427,49 @@ BIC(HAR)
 
 # Specific HAR model with day of the week factor
 HAR_dow <- lm(sqrt(ntable[(long + 1) : L, RV]) ~ sqrt(ntable[(long) : (L - 1), RV]) + 
-            rvols[(long - short + 1) : (Ls - 1)] + rvoll[1 : (Ll - 1)] + dow[(long + 1) : L])
+            rvols[(long - short + 1) : (Ls - 1)] + rvoll[1 : (Ll - 1)] + ntable[(long + 1) : L, dow])
 summary(HAR_dow)
 BIC(HAR_dow)
 
 # Specific HAR model with working days dummy
 HAR_wd <- lm(sqrt(ntable[(long + 1) : L, RV]) ~ sqrt(ntable[(long) : (L - 1), RV]) + 
-                rvols[(long - short + 1) : (Ls - 1)] + rvoll[1 : (Ll - 1)] + wd[(long + 1) : L])
+                rvols[(long - short + 1) : (Ls - 1)] + rvoll[1 : (Ll - 1)] + ntable[(long + 1) : L, wd])
 summary(HAR_wd)
 BIC(HAR_wd)
 
-rm()
+rm(rvols, rvoll)
+
+# Moving averages of MedRV(3)
+nds <- unlist(lapply(lapply(short : L, function(t){return(ntable[(t - (short - 1)) : t, medRV])}), mean))
+ndl <- unlist(lapply(lapply(long : L, function (t) {return(ntable[(t - (long - 1)) : t, medRV])}), mean))
+
+# Moving average of volatility
+medvols <- sqrt(nds)
+medvoll <- sqrt(ndl)
+
+rm(nds, ndl)
+
+# HAR MedRV model
+medHAR <- lm(sqrt(ntable[(long + 1) : L, medRV]) ~ sqrt(ntable[(long) : (L - 1), medRV]) + 
+             medvols[(long - short + 1) : (Ls - 1)] + medvoll[1 : (Ll - 1)])
+summary(medHAR)
+BIC(medHAR)
+
+# HAR MedRV model with day of the week factor
+medHAR_dow <- lm(sqrt(ntable[(long + 1) : L, medRV]) ~ sqrt(ntable[(long) : (L - 1), medRV]) + 
+                 medvols[(long - short + 1) : (Ls - 1)] + medvoll[1 : (Ll - 1)] + 
+                 ntable[(long + 1) : L, dow])
+summary(medHAR_dow)
+BIC(medHAR_dow)
+
+# HAR MedRV model with working days dummy
+medHAR_wd <- lm(sqrt(ntable[(long + 1) : L, medRV]) ~ sqrt(ntable[(long) : (L - 1), medRV]) + 
+                medvols[(long - short + 1) : (Ls - 1)] + medvoll[1 : (Ll - 1)] + 
+                ntable[(long + 1) : L, wd])
+summary(medHAR_wd)
+BIC(medHAR_wd)
+
+rm(medvols, medvoll, Ls, Ll, long)
 
 ########## GARCH(1, 1) Comparison ##########
 # GARCH(1, 1) model for the data
@@ -498,44 +480,101 @@ garch <- rugarch::ugarchfit(garchs, ntable[, ret])
 rm(garchs)
 
 # Comparison between HAR and GARCH(1, 1)
-plot.ts(sqrt(ntable$RV[23 : L]), ylab = NA, 
+plot.ts(sqrt(ntable$RV[(long + 1) : L]), ylab = NA, 
         main = paste0(coin, ' Estimated Volatility Comparison'), font.main = 1)
-lines(garch@fit$sigma[23 : L], col = 'blue')
+lines(garch@fit$sigma[(long + 1) : L], col = 'blue')
 lines(HAR$fitted.values, col = 'red')
 lines(medHAR$fitted.values, col = 'green')
 legend('topleft', ncol = 4, col = c('black', 'blue', 'red', 'green'), lwd = 2, bty = 'n',
        legend = c('RV', 'GARCH(1, 1)', 'HAR', 'MedRV HAR'))
 
 # Mean square error and mean absolute error
-mu <- sqrt(ntable$RV[23 : L])
+mu <- sqrt(ntable$RV[(long + 1) : L])
 
 HAR_fv <- HAR$fitted.values
+dowHAR_fv <- HAR_dow$fitted.values
+wdHAR_fv <- HAR_wd$fitted.values
 medHAR_fv <- medHAR$fitted.values
-GARCH_fv <- garch@fit$sigma[23 : L]
+dowmedHAR_fv <- medHAR_dow$fitted.values
+wdmedHAR_fv <- medHAR_wd$fitted.values
+GARCH_fv <- garch@fit$sigma[(long + 1) : L]
 
 mse_HAR <- mean((mu - HAR_fv)^2)
+mse_dowHAR <- mean((mu - dowHAR_fv)^2)
+mse_wdHAR <- mean((mu - wdHAR_fv)^2)
 mse_medHAR <- mean((mu - medHAR_fv)^2)
+mse_dowmedHAR <- mean((mu - dowmedHAR_fv)^2)
+mse_wdmedHAR <- mean((mu - wdmedHAR_fv)^2)
 mse_GARCH <- mean((mu - GARCH_fv)^2)
 
 mae_HAR <- mean(abs(mu - HAR_fv))
+mae_dowHAR <- mean(abs(mu - dowHAR_fv))
+mae_wdHAR <- mean(abs(mu - wdHAR_fv))
 mae_medHAR <- mean(abs(mu - medHAR_fv))
+mae_dowmedHAR <- mean(abs(mu - dowmedHAR_fv))
+mae_wdmedHAR <- mean(abs(mu - wdmedHAR_fv))
 mae_GARCH <- mean(abs(mu - GARCH_fv))
 
-eix <- matrix(ncol = 3, nrow = 3)
+rm(long, HAR_fv, dowHAR_fv, wdHAR_fv, medHAR_fv, dowmedHAR_fv, wdmedHAR_fv, GARCH_fv)
+
+eix <- matrix(ncol = 3, nrow = 8)
 
 colnames(eix) <- c('Model', 'MSE', 'MAE')
-eix[, 1] <- c('HAR-RV', 'HAR-MedRV', 'GARCH(1, 1)')
+eix[, 1] <- c('HAR-RV', 'HAR-RV-DoW', 'HAR-RV-WD', 'HAR-MedRV', 'HAR-MedRV-DoW',
+              'HAR-MedRV-WD', 'GARCH(1, 1)')
 
 eix[1, 2] <- mse_HAR * 1000
 eix[1, 3] <- mae_HAR * 1000
-eix[2, 2] <- mse_medHAR * 1000
-eix[2, 3] <- mae_medHAR * 1000
-eix[3, 2] <- mse_GARCH * 1000
-eix[3, 3] <- mae_GARCH * 1000
+eix[2, 2] <- mse_dowHAR * 1000
+eix[2, 3] <- mae_dowHAR * 1000
+eix[3, 2] <- mse_wdHAR * 1000
+eix[3, 3] <- mae_wdHAR * 1000
+eix[4, 2] <- mse_medHAR * 1000
+eix[4, 3] <- mae_medHAR * 1000
+eix[5, 2] <- mse_dowmedHAR * 1000
+eix[5, 3] <- mae_dowmedHAR * 1000
+eix[6, 2] <- mse_wdmedHAR * 1000
+eix[6, 3] <- mae_wdmedHAR * 1000
+eix[7, 2] <- mse_GARCH * 1000
+eix[7, 3] <- mae_GARCH * 1000
+
+rm(mse_HAR, mae_HAR, mse_dowHAR, mae_dowHAR, mse_wdHAR, mae_wdHAR, mse_medHAR,
+   mae_medHAR, mse_dowmedHAR, mae_dowmedHAR, mse_wdmedHAR, mae_wdmedHAR,
+   mse_GARCH, mae_GARCH, mse_RVGARCH, mae_RVGARCH)
 
 View(eix)
 
+openxlsx::write.xlsx(eix, file = paste0(path, 'eix_', coin, f, '.xlsx'))
 
+# Plot histogram of the standardized returns
+hist(ntable[, ret], main = 'Distribution of BTC Standardized Log Returns', xlab = NA, prob = TRUE, 
+     breaks = 30, font.main = 1, cex.main = 1, xlim = c(-0.2, 0.2))
+curve(dnorm(x, mean = mean(ntable[, ret]), sd = sd(ntable[, ret])), col = 'darkblue', 
+      lwd = 2, add = TRUE, yaxt = 'n')
+
+# Histogram of the residuals
+hist(residuals(HAR), breaks = 50, font.main = 1, main = 'Histogram of the Residuals of the HAR Model',
+     prob = TRUE, xlab = NA)
+curve(dnorm(x, mean = mean(residuals(HAR)), sd = sd(residuals(HAR))), col = "darkblue", add = TRUE, lwd = 2)
+
+# Normal Q-Q Plot for the residuals
+qqnorm(residuals(HAR), font.main = 1, cex.main = 1.1,
+       main = 'Normal Q-Q Plot for the Residuals of the HAR Model')
+qqline(residuals(HAR))
+
+# Jarque Bera test for the residuals
+tseries::jarque.bera.test(residuals(HAR))
+
+# ACF of the residuals
+acf(residuals(HAR), main = 'ACF for Residuals of HAR Model', ylab = NA)
+# PACF of the residuals
+pacf(residuals(HAR), main = 'PACF for Residuals of HAR Model', ylab = NA)
+
+# Ljung-Box test for the residuals (H0: independently distributed data)
+Box.test(residuals(HAR), type = 'Ljung-Box', lag = 5)
+
+# Breusch-Pagan test (H0: homoskedasticity)
+lmtest::bptest(HAR)
 
 
 
