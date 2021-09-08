7de52df67899c159cf600404e9aaab6c5fc53976
rcviale <77018783+rcviale@users.noreply.github.com>
2021-09-08 15:55:04 -0300

    Add PCA-based weights computation. Add PCA-based Fama-MacBeth regression. Create function for first step Fama-MacBeth regression, ignoring zeroes in the beggining of each series. Create function to check for any zeroes in the middle of each series.

--- R/fmb2.R
+++ R/fmb2.R
@@ -3,9 +3,12 @@ fmb2 <- function(data, col_date = date, mkt_data = c(mkt_ret, mkt_rvol), beta1, 
   data %>% 
     # Take market return and volatility out of the data set
     dplyr::select(-{{mkt_data}}) %>% 
-    tidyr::pivot_longer(cols = -{{col_date}}, names_to = "assets", values_to = "rets") %>% 
+    # Pivot longer
+    tidyr::pivot_longer(cols = -{{col_date}}, names_to = "assets", values_to = "rets") %>%
+    # Nest data
     tidyr::nest(data = c(assets, rets)) %>% 
+    # Regression
     dplyr::mutate(reg      = map(.x = data, .f = ~ lm(formula = rets ~ beta1 + beta2, data = .x)), 
            reg_tidy = map(.x = reg, .f = broom::tidy)) %>% 
-    unnest(reg_tidy)
+    tidyr::unnest(reg_tidy)
 }
