70882fc633f019b364fb3a3984d36076fa6e251f
rcviale <77018783+rcviale@users.noreply.github.com>
2021-10-03 17:03:04 -0300

    Fix pre_fmb function. Add fmb_t function.

--- R/fmb2.R
+++ R/fmb2.R
@@ -1,14 +1,19 @@
 # Function to compute 2nd Fama-MacBeth regression
-fmb2 <- function(data, col_date = date, mkt_data = c(mkt_ret, mkt_rvol), beta1, beta2){
+fmb2 <- function(data, beta1, beta2, K = 10){
   data %>% 
     # Take market return and volatility out of the data set
-    dplyr::select(-{{mkt_data}}) %>% 
+    dplyr::select(-c(mkt_ret, mkt_rvol)) %>% 
     # Pivot longer
-    tidyr::pivot_longer(cols = -{{col_date}}, names_to = "assets", values_to = "rets") %>%
+    tidyr::pivot_longer(cols = -date, names_to = "assets", values_to = "rets") %>%
     # Nest data
     tidyr::nest(data = c(assets, rets)) %>% 
-    # Regression
-    dplyr::mutate(reg      = map(.x = data, .f = ~ lm(formula = rets ~ beta1 + beta2, data = .x)), 
-           reg_tidy = map(.x = reg, .f = broom::tidy)) %>% 
-    tidyr::unnest(reg_tidy)
+    # Nunmber of series in that day
+    mutate(n        = map_dbl(.x = data, .f = ~10 - sum(is.na(.x)))) %>%
+    # Filter only days with K series
+    filter(n == K) %>% 
+    # Run regression
+    mutate(reg      = map(.x = data, .f = ~lm(.x$rets ~ beta1 + beta2)),
+           reg_tidy = map(.x = reg, .f = broom::tidy),
+           rsq      = map_dbl(.x = reg, .f = ~summary(.x)$r.squared)) %>% 
+    unnest(reg_tidy)
 }
--- R/pre_fmb.R
+++ R/pre_fmb.R
@@ -8,7 +8,7 @@ pre_fmb <- function(data, date = date, mkt_ret = mkt_ret, mkt_rvol = mkt_rvol){
       tidyr::nest(data = c(date, {{mkt_ret}}, {{mkt_rvol}}, rets)) %>% 
       # Column with first observation that should be considered in the regression, 
       # then verifying if there are any other zeroes besides the ones before each series start
-      dplyr::mutate(t0        = map_dbl(.x = data, .f = ~length(which(.x$rets == 0)) + 1),
+      dplyr::mutate(t0        = map_dbl(.x = data, .f = ~sum(is.na(.x$rets)) + 1),
                     other_0   = map2_lgl(.x = data, .y = t0, .f = ~any(.x$rets[.y : 1430] == 0))) %>% 
       # Check if any value in the column t0 == 0
       summarise(any(other_0 == TRUE)) %>% 
