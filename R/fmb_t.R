d37f60b2f06ff2594279ee59ebfb8681db7b2b11
rcviale <77018783+rcviale@users.noreply.github.com>
2021-10-03 17:03:32 -0300

    Add fmb_t function.

---
+++ R/fmb_t.R
@@ -0,0 +1,14 @@
+fmb_t <- function(data, lambda1, lambda2){
+  data %>% 
+    filter(term == lambda1) %>% 
+    select(estimate) %>% 
+    t.test() -> cf1
+  data %>% 
+    filter(term == lambda2) %>% 
+    select(estimate) %>% 
+    t.test() -> cf2
+  tibble(coef   = c("lambda1", "lambda2"),
+         t_stat = c(cf1$statistic, cf2$statistic),
+         p_val  = c(cf1$p.value, cf2$p.value),
+         x_mean = c(cf1$estimate, cf2$estimate))
+}
