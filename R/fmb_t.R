bd8fbdf619badea2fce6ff4633162ab1c9d9a6ce
rcviale <77018783+rcviale@users.noreply.github.com>
2021-10-17 22:31:06 -0300

    Add intercept test.

--- R/fmb_t.R
+++ R/fmb_t.R
@@ -1,14 +1,34 @@
 fmb_t <- function(data, lambda1, lambda2){
   data %>% 
+    filter(term == "(Intercept)") %>% 
+    select(estimate) %>% 
+    t.test() -> cf0
+  data %>% 
+    filter(term == "(Intercept)") %>% 
+    select(estimate) %>% 
+    unlist() %>% 
+    sd() -> sd0
+  data %>% 
     filter(term == lambda1) %>% 
     select(estimate) %>% 
     t.test() -> cf1
   data %>% 
+    filter(term == lambda1) %>% 
+    select(estimate) %>% 
+    unlist() %>% 
+    sd() -> sd1
+  data %>% 
     filter(term == lambda2) %>% 
     select(estimate) %>% 
     t.test() -> cf2
-  tibble(coef   = c("lambda1", "lambda2"),
-         x_mean = c(cf1$estimate, cf2$estimate),
-         t_stat = c(cf1$statistic, cf2$statistic),
-         p_val  = c(cf1$p.value, cf2$p.value))
+  data %>% 
+    filter(term == lambda2) %>% 
+    select(estimate) %>% 
+    unlist() %>% 
+    sd() -> sd2
+  tibble(coef   = c("lambda0", "lambda1", "lambda2"),
+         x_mean = c(cf0$estimate, cf1$estimate, cf2$estimate),
+         sd_er  = c(sd0, sd1, sd2),
+         t_stat = c(cf0$statistic, cf1$statistic, cf2$statistic),
+         p_val  = c(cf0$p.value, cf1$p.value, cf2$p.value))
 }
